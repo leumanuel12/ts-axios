200b9e72d55e05817525b1684c5b413b19c5ac18
leumanuel12 <cantiveros12@gmail.com>
2023-01-17 20:45:37 +0800

    setup chartjs line chart for displaying crypto prices at 31days fixed

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,37 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import CryptoSummary from './components/CryptoSummary';
 import { Crypto } from './Types';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import { Line } from "react-chartjs-2";
+import type { ChartData, ChartOptions } from 'chart.js';
+import moment from 'moment';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function App() {
   const [cryptos, setCryptos] = useState<Crypto[] | null>(null);
   const [selected, setSelected] = useState<Crypto | null>();
+  const [data, setData] = useState<ChartData<'line'>>();
+  const [options, setOptions] = useState<ChartOptions<'line'>>({
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  });
 
   useEffect(() => {
     const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
@@ -27,8 +54,31 @@ export default function App() {
           const c = cryptos?.find( (x) => x.id === e.target.value );
           setSelected(c);
           //console.log(c);
+
+          axios.get(`https://api.coingecko.com/api/v3/coins/${c?.id}/market_chart?vs_currency=usd&days=30&interval=daily`)
+          .then( (response) => {
+            //console.log(response.data)
+            const priceLabel = response.data.prices.map( (price: number[]) => {
+               return moment.unix(price[0] / 1000).format('MM-DD-YYYY')
+              } );
+            const priceData = response.data.prices.map( (price: number[]) => { return price[1] } );
+            //console.log(label)
+            setData({
+              labels: priceLabel,
+              datasets: [
+                {
+                  label: 'Dataset 1',
+                  data: priceData,
+                  borderColor: 'rgb(255, 99, 132)',
+                  backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                }
+              ],
+            })
+          } )
         }}
+        defaultValue="value"
         >
+          <option value="default">Select an option</option>
         {cryptos ? cryptos.map((crypto) => {
           return <option key={crypto.id} value={crypto.id}>{crypto.name}</option>
         }) : null}
@@ -36,40 +86,8 @@ export default function App() {
 
       {selected ? <CryptoSummary crypto={selected} /> : null}
 
+      {data ? <div className="max-w-2xl"><Line options={options} data={data} /></div> : null}
+
     </div>
   );
 }
-
-/*
-<table className="mt-4">
-        <thead>
-          <tr>
-            <td className='border border-gray-400 px-2'>ID</td>
-            <td className='border border-gray-400 px-2'>Symbol</td>
-            <td className='border border-gray-400 px-2'>Name</td>
-            <td className='border border-gray-400 px-2'>Current Price</td>
-            <td className='border border-gray-400 px-2'>High 24H</td>
-            <td className='border border-gray-400 px-2'>Low 24H</td>
-            <td className='border border-gray-400 px-2'>ATH</td>
-            <td className='border border-gray-400 px-2'>ATL</td>
-            </tr>
-        </thead>
-        <tbody>
-
-      {cryptos ? cryptos.map( (crypto) => {
-        return (
-          <tr key={crypto.id}>
-            <td className='border border-gray-400 px-2'>{crypto.id}</td>
-            <td className='border border-gray-400 px-2'>{crypto.symbol}</td>
-            <td className='border border-gray-400 px-2'>{crypto.name}</td>
-            <td className='border border-gray-400 px-2'>{crypto.current_price}</td>
-            <td className='border border-gray-400 px-2'>{crypto.high_24h}</td>
-            <td className='border border-gray-400 px-2'>{crypto.low_24h}</td>
-            <td className='border border-gray-400 px-2'>{crypto.ath}</td>
-            <td className='border border-gray-400 px-2'>{crypto.atl}</td>
-          </tr>
-        )
-      } ) : null}
-      </tbody>
-      </table>
-*/
